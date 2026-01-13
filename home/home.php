76a40c6dba74de3f81f6a0e562f0ad8061460b72
Yona1e <161945495+Yona1e@users.noreply.github.com>
2026-01-13 19:24:38 +0800

    Update home.php

--- home/home.php
+++ home/home.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../resident-side/style/make-reservation.css">
+    <link rel="stylesheet" href="../resident-side/make-reservation.css">
     <!-- Google Material Symbols -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
